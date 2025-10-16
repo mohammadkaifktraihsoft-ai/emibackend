361f8292fb5250b9f8ea09cdd282f41ba4f6361c
mohammadkaifktraihsoft-ai <mohammad.kaif.kt.raihsoft@gmail.com>
2025-10-16 23:06:40 +0530

    Restoreing

--- emiapp/urls.py
+++ emiapp/urls.py
@@ -1,15 +1,24 @@
-from rest_framework import routers
-from .views import CustomerViewSet, EMIViewSet,PaymentViewSet ,ping
 from django.urls import path, include
+from rest_framework import routers
+from .views import (
+    CustomerViewSet,
+    EMIViewSet,
+    PaymentViewSet,
+    UserProfileViewSet,
+    SignUpView,
+    LoginView,
+    ping,
+)
 
 router = routers.DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 router.register(r'emis', EMIViewSet)
 router.register(r'payments', PaymentViewSet)
-
+router.register(r'userprofile', UserProfileViewSet, basename='userprofile')
 
 urlpatterns = [
-    path('ping/', ping),           # your ping endpoint
-    path('', include(router.urls)) # include all router URLs
-    
+    path('ping/', ping),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('', include(router.urls)),
 ]
