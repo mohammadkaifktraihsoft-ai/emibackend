fd443271d4f81ea78f3a1ec2a94858848b661763
mohammadkaifktraihsoft-ai <mohammad.kaif.kt.raihsoft@gmail.com>
2025-10-20 13:05:03 +0530

    login

--- emiapp/models.py
+++ emiapp/models.py
@@ -10,6 +10,7 @@ class UserProfile(models.Model):
     shop_name = models.CharField(max_length=255, blank=True)
     distributor_name = models.CharField(max_length=255, blank=True)
     distributor_contact = models.CharField(max_length=20, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
